4b510e1f628ecf7d182d4ff6f353ff59b0772657
amirkhan1092 <amirkhan1092@gmail.com>
2023-08-30 00:59:59 +0530

    rectangle area

---
+++ Programs/Sequential Programs/rectangle_area.py
@@ -0,0 +1,10 @@
+# write a program to find the area of a rectangle
+# input section
+length = int(input("Enter the length of the rectangle: "))
+breadth = int(input("Enter the breadth of the rectangle: "))
+
+# logic section
+area = length * breadth
+
+# output section
+print("The area of the rectangle is {0}".format(area))
