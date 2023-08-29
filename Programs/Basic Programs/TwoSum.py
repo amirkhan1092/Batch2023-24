a5f5bb35a61bd0ac94111ea08d03d6b046251f9d
amirkhan1092 <amirkhan1092@gmail.com>
2023-08-30 00:49:35 +0530

    Addition of two numbers

---
+++ Programs/Basic Programs/TwoSum.py
@@ -0,0 +1,9 @@
+# input section 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# logic Section 
+sum = num1 + num2
+
+# output section
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
