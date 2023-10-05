3eb825c2747e9287a680dbad2af2f60bb3a7f1a6
amirkhan1092 <amirkhan1092@gmail.com>
2023-10-05 13:11:45 +0530

    basics of list

---
+++ Programs/list_programs/basics.py
@@ -0,0 +1,14 @@
+# list : ordered collection of object 
+# operation with list
+
+# Assignment with list
+'''
+1. item assignment
+2. item deletion
+'''
+ls = [4, 6, 8]
+ls2 = ls.copy()
+ls2.clear()
+print('ls', ls, id(ls))
+print('ls2', ls2, id(ls2))
+
