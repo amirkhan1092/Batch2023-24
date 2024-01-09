e5c5582cd70be7b1dd1714ba12c7de7937162d79
amirkhan1092 <amirkhan1092@gmail.com>
2024-01-09 09:44:59 +0530

    latest

---
+++ projects beginner/tic_tac_basic.py
@@ -0,0 +1,26 @@
+# Tic Tac Toe
+
+def print_stack(lst):
+    print(f'''
+{lst[0]}|{lst[1]}|{lst[2]}
+----------
+{lst[3]}|{lst[4]}|{lst[5]}
+----------
+{lst[6]}|{lst[7]}|{lst[8]}''')
+    
+
+# main code 
+moves = [' '] * 9
+print_stack(moves)
+
+choice = 'X'
+while 1:
+    m_id = int(input(f'Turn {choice} Enter the move position (0-8) '))
+    if moves[m_id] == ' ':
+        moves[m_id] = choice
+    else:
+        print('location already used ')
+        continue
+    print_stack(moves)
+
+    choice = 'X' if choice == '0' else '0'
