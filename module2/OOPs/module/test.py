b20a0fb35a4055e6c10885ed3ecb0bf7955376d0
amirkhan1092 <amirkhan1092@gmail.com>
2024-01-25 09:15:57 +0530

    __name__

--- module2/OOPs/module/test.py
+++ module2/OOPs/module/test.py
@@ -1,14 +1,13 @@
 def add(a, b):
     return a + b
-
-
 def sub(a, b):
     return a - b
 
-
-# main code 
+# main code
+val = 100
 if __name__ == '__main__':
     num1 = int(input('Enter the first number '))
-    num2 = int(input('enter the second number '))
+    num2 = int(input('Enter the second number '))
+
     out = add(num1, num2)
     print(f'Addition of {num1} and {num2} is {out}')
