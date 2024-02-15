e364a9c26d6db4a025ee83c99e872a23a3fb3a18
amirkhan1092 <amirkhan1092@gmail.com>
2024-02-15 09:38:20 +0530

    to do list

--- projects beginner/GUI/todo2.py
+++ projects beginner/GUI/todo2.py
@@ -1,37 +1,35 @@
-# To Do List
 import tkinter as tk
 
-class ToDo:
-    def __init__(self, win) -> None:
-        self.win = win
-        win.title('To Do List')
-        win.geometry('400x300')
+# To Do List
+class to_do_list:
+    def __init__(self,mainwindow) -> None:
+        self.mainwindow = mainwindow
+        
+        self.en1 = tk.Entry(mainwindow)
+        self.en1.grid(row=0, column=0)
 
-        self.entry = tk.Entry(win)
-        self.entry.grid(row=0, column=0)
-        tk.Button(win, text='Add Item', command=self.add_item).grid(row=0, column=1)
+        self.bt1 = tk.Button(mainwindow,text='Add Item', command=self.add_item)
+        self.bt1.grid(row=0, column=1)
 
-        self.list_box = tk.Listbox(win, width=40)
+        self.list_box = tk.Listbox(width=30)
         self.list_box.grid(row=1, column=0, columnspan=2)
 
-        tk.Button(win, text='Delete Item', command=self.delete_item).grid(row=2, column=0)
-        tk.Button(win, text='Delete All', command=self.clear_list).grid(row=2, column=1)
+        self.bt2 = tk.Button(mainwindow,text='Delete Item', command=self.del_item)
+        self.bt2.grid(row=2, column=0)
+
+        self.bt3 = tk.Button(mainwindow, text='Delete All Items', command=self.del_all_item)
+        self.bt3.grid(row=2, column=1)
 
     def add_item(self):
-        data = self.entry.get()
+        data = self.en1.get()
         self.list_box.insert(tk.END, data)
-        self.entry.delete(0, tk.END)
-    def delete_item(self):
-        sel_index = self.list_box.curselection()
-        self.list_box.delete(sel_index)
-    def clear_list(self):
+    
+    def del_item(self):
+        self.list_box.delete(self.list_box.curselection())
+    
+    def del_all_item(self):
         self.list_box.delete(0, tk.END)
 
-
-
-
-
-# main
 root = tk.Tk()
-app = ToDo(root)
+to_do_list(root)
 root.mainloop()
